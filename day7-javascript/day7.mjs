113213673276cf899bb0f4b0ce09dd95f13adb70
Georgii Perepechko <georgiiperepechko@gmail.com>
2022-12-08 13:13:52 +0000

    use node:path's join

--- day7-javascript/day7.mjs
+++ day7-javascript/day7.mjs
@@ -1,26 +1,19 @@
 import { open } from 'node:fs/promises';
-import path from 'node:path';
-
-const file = await open('input.txt');
+import { join } from 'node:path';
 
 // flat object of all directories using full path as a key
 const dirs = {};
-
 // stack of directories leading to current dir
 let dirsStack = [];
 
-const getDirName = () =>
-    dirsStack.join('/').slice(dirsStack.length === 1 ? 0 : 1);
-
-const getInitialDirState = () => ({ filesSize: 0, subdirectories: [] });
-
+const file = await open('input.txt');
 // O(n) time, O(d) space where d is number of directories
 for await (const line of file.readLines()) {
     const splitLine = line.split(' ');
     if (splitLine[0] === '$') {
         const [_, commandName, argument] = splitLine;
         if (commandName === 'ls') {
-            dirs[getDirName()] = getInitialDirState();
+            dirs[join(...dirsStack)] = { filesSize: 0, subdirectories: [] };
         } else if (commandName === 'cd') {
             switch (argument) {
                 case '..':
@@ -39,41 +32,30 @@ for await (const line of file.readLines()) {
     } else {
         if (line.startsWith('dir')) {
             const [_dirType, dirName] = splitLine;
-            dirs[getDirName()].subdirectories.push(dirName);
+            dirs[join(...dirsStack)].subdirectories.push(dirName);
         } else {
             const [fileSizeStr, _fileName] = splitLine;
-            dirs[getDirName()].filesSize += +fileSizeStr;
+            dirs[join(...dirsStack)].filesSize += +fileSizeStr;
         }
     }
 }
 
-const getFullSubdirectoryName = (pathToDir, subdirectoryName) =>
-    `${pathToDir === '/' ? '' : pathToDir}/${subdirectoryName}`;
-
 // O(d) time, O(d) space where d is number of directories
-function dfsGetSize(pathToDir) {
+function dfsUpdateDirSize(pathToDir) {
     const dir = dirs[pathToDir];
-    // not a huge fan of:
-    // a) doing it recursively
-    // b) mutating objects as I do it
-    // probably would've refactored the code if I had a bit more time
-    dir.dirSize = dir.subdirectories.reduce(
-        (acc, subdirectoryName) =>
-            acc +
-            dfsGetSize(getFullSubdirectoryName(pathToDir, subdirectoryName)),
-        dir.filesSize
-    );
-    return dir.dirSize;
+    dir.dirSize = dir.subdirectories.reduce((acc, subdirectoryName) => {
+        const pathToSubdirectory = join(pathToDir, subdirectoryName);
+        dfsUpdateDirSize(pathToSubdirectory);
+        return acc + dirs[pathToSubdirectory].dirSize;
+    }, dir.filesSize);
 }
+dfsUpdateDirSize('/');
 
 let smallDirsTotalSize = 0;
-const totalUsedSpace = dfsGetSize('/');
-const memoryAvailable = 70_000_000 - totalUsedSpace;
+const rootDirInfo = { dirName: '/', dirSize: dirs['/'].dirSize };
+const memoryAvailable = 70_000_000 - rootDirInfo.dirSize;
 const memoryToFree = 30_000_000 - memoryAvailable;
-let currentBestSolution = {
-    dirName: '/',
-    dirSize: totalUsedSpace
-};
+let currentBestSolution = rootDirInfo;
 
 // O(d) time where d is number of directories, O(1) space
 for (const [dirName, { dirSize }] of Object.entries(dirs)) {
