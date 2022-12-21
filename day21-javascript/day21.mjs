7dc0c49cae89d23047d378e7e41abfc4274ed0b1
Georgii Perepechko <georgiiperepechko@gmail.com>
2022-12-21 23:35:34 +0000

    refactor

--- day21-javascript/day21.mjs
+++ day21-javascript/day21.mjs
@@ -1,62 +1,50 @@
 import fs from 'node:fs';
 import readline from 'node:readline';
-
-function inferMissingData(sign, first, second, result) {
-    const isFirstFunc = typeof first === 'function';
+const isCb = value => typeof value === 'function';
+function inferX(sign, left, right, result) {
     switch (sign) {
         case '+':
-            if (!result) return first + second;
-            return isFirstFunc
-                ? first(result - second)
-                : second(result - first);
+            if (!result) return left + right;
+            return isCb(left) ? left(result - right) : right(result - left);
         case '-':
-            if (!result) return first - second;
-            return isFirstFunc
-                ? first(result + second)
-                : second(first - result);
+            if (!result) return left - right;
+            return isCb(left) ? left(result + right) : right(left - result);
         case '/':
-            if (!result) return first / second;
-            return isFirstFunc
-                ? first(result * second)
-                : second(first / result);
+            if (!result) return left / right;
+            return isCb(left) ? left(result * right) : right(left / result);
         case '*':
-            if (!result) return first * second;
-            return isFirstFunc
-                ? first(result / second)
-                : second(result / first);
+            if (!result) return left * right;
+            return isCb(left) ? left(result / right) : right(result / left);
     }
 }
-
 function setIntoMonkeysMap(monkeys, line, overrides) {
     const [name, work] = line.split(': ');
     const [m1, sign, m2] = work.split(' ');
-    monkeys[name] = overrides?.[name]
-        ? () => overrides[name](monkeys, m1, m2)
-        : !sign
-        ? () => Number.parseInt(work)
-        : () => {
-              const results = [monkeys[m1](), monkeys[m2]()];
-              if (results.some(r => typeof r === 'function'))
-                  return target => inferMissingData(sign, ...results, target);
-              return inferMissingData(sign, ...results);
-          };
+    if (overrides?.[name]) {
+        monkeys[name] = () => overrides[name](monkeys, m1, m2);
+    } else if (sign === undefined) {
+        monkeys[name] = () => Number.parseInt(work);
+    } else {
+        monkeys[name] = () => {
+            const values = [monkeys[m1](), monkeys[m2]()];
+            if (values.some(isCb)) return res => inferX(sign, ...values, res);
+            return inferX(sign, ...values);
+        };
+    }
 }
-
-const monkeys1 = Object.create(null);
-const monkeys2 = Object.create(null);
 const task2Overrides = {
-    humn: _ => x => x,
+    humn: _ => res => res,
     root: (monkeys, m1, m2) => {
-        const result1 = monkeys[m1]();
-        const result2 = monkeys[m2]();
-        if (typeof result1 === 'function') return result1(result2);
-        if (typeof result2 === 'function') return result2(result1);
+        const leftValue = monkeys[m1]();
+        const rightValue = monkeys[m2]();
+        return isCb(leftValue) ? leftValue(rightValue) : rightValue(leftValue);
     }
 };
 const readlineInterfaceArg = { input: fs.createReadStream('input.txt') };
+const monkeyRosters = [{}, {}];
 for await (const line of readline.createInterface(readlineInterfaceArg)) {
-    setIntoMonkeysMap(monkeys1, line);
-    setIntoMonkeysMap(monkeys2, line, task2Overrides);
+    setIntoMonkeysMap(monkeyRosters[0], line);
+    setIntoMonkeysMap(monkeyRosters[1], line, task2Overrides);
 }
-console.log(`Task 1 result is: ${monkeys1.root()}`);
-console.log(`Task 2 result is: ${monkeys2.root()}`);
+console.log(`Task 1 result is: ${monkeyRosters[0].root()}`);
+console.log(`Task 2 result is: ${monkeyRosters[1].root()}`);
