08145ed113468ccf38cdac481797e1f9c74d6fa5
Georgii Perepechko <georgiiperepechko@gmail.com>
2022-12-25 06:00:11 +0000

    refactor

--- day25-javascript/day25.mjs
+++ day25-javascript/day25.mjs
@@ -1,39 +1,27 @@
 import fs from 'node:fs';
-import readline from 'node:readline';
-const snafuNotationChars = ['=', '-', '0', '1', '2'];
 
-function snafuToDecimal(line) {
-    return line
+const snafuNotationChars = ['=', '-', '0', '1', '2'];
+const snafuValues = { '-': -1, '=': -2 };
+
+const snafuToDecimal = line =>
+    line
         .split('')
         .reverse()
-        .map(digit => {
-            if (digit == '-') {
-                return -1;
-            } else if (digit == '=') {
-                return -2;
-            }
-            return Number.parseInt(digit);
-        })
+        .map(digit => snafuValues[digit] ?? Number.parseInt(digit))
         .map((d, i) => d * 5 ** i)
         .reduce((acc, v) => acc + v);
-}
 
 function decimalToSnafu(num) {
     let result = [];
     while (num > 0) {
-        const index = (num + 2) % 5;
+        const remainder = (num + 2) % 5;
         num = Math.floor((num + 2) / 5);
-        result.push(snafuNotationChars[index]);
+        result.push(snafuNotationChars[remainder]);
     }
-
     return result.reverse().join('');
 }
 
-(async function main() {
-    let sum = 0;
-    for await (const line of readline.createInterface({
-        input: fs.createReadStream('input.txt')
-    }))
-        sum += snafuToDecimal(line);
-    console.log(`Final result is: ${decimalToSnafu(sum)} (${sum})`);
-})();
+let sum = 0;
+for (const line of fs.readFileSync('input.txt').toString().split('\n'))
+    sum += snafuToDecimal(line);
+console.log(`Final result is: ${decimalToSnafu(sum)} (${sum})`);
